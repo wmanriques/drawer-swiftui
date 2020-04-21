8f936a0c9417156a809be193b0095f0fc648041e
Manrique Salazar, Williams J <williams.manrique@one.verizon.com>
2020-04-21 18:38:41 -0500

    Initial Commit

---
+++ DrawerView/DrawerView.h
@@ -0,0 +1,19 @@
+//
+//  DrawerView.h
+//  DrawerView
+//
+//  Created by wsk on 4/21/20.
+//  Copyright © 2020 wsk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DrawerView.
+FOUNDATION_EXPORT double DrawerViewVersionNumber;
+
+//! Project version string for DrawerView.
+FOUNDATION_EXPORT const unsigned char DrawerViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawerView/PublicHeader.h>
+
+
